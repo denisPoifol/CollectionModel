1af3fd020cefe2813df6c977899e8a33afb5847e
Denis Poifol <denis.poifol@gmail.com>
2020-08-27 12:27:07 +0200

    Update comment for SupplementaryViewKey

--- Core/Sources/SupplementaryViewKey.swift
+++ Core/Sources/SupplementaryViewKey.swift
@@ -7,7 +7,8 @@
 
 import Foundation
 
-/// Suplementary view are identified by an index path and their kind which is a string
+/// Suplementary view are identified by an index path and their kind which is a string, `SupplementaryKey`
+/// is a simple struct that regroup both information to easily access your SupplementaryView viewModel.
 public struct SupplementaryViewKey: Hashable {
 
     public init(indexPath: IndexPath, kind: String) {
