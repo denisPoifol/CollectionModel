ad1b8065ce45f58186c4d1e17f9e57223fba2ef7
Denis Poifol <denisPoifol@users.noreply.github.com>
2021-03-08 09:02:16 +0100

    feature/fix differentiable conformance (#5)
    * Fix equality of two CollectionViewSectionViewModel
    
    * Fix equality of two TableViewSectionViewModel
    
    * Simplify conditions in order to make CollectionViewSectionViewModel conforming to DifferentiableSection
    
    * Update comments
    
    Co-authored-by: Roland Borgese <roland.borgese@fabernovel.com>

--- Animation/Sources/CollectionViewModel+DifferenceKit.swift
+++ Animation/Sources/CollectionViewModel+DifferenceKit.swift
@@ -1,18 +1,28 @@
 import CollectionModelCore
 import DifferenceKit
 
-
-extension CollectionViewSectionViewModel: ContentEquatable where
-    CellViewModel: Hashable & Differentiable,
-    SupplementaryViewModel: Hashable & Differentiable {
-    public func isContentEqual(to source: Self) -> Bool {
-        return id.isContentEqual(to: source.id)
+extension Dictionary: ContentEquatable where Value: ContentEquatable {
+    public func isContentEqual(to source: [Key: Value]) -> Bool {
+        guard keys == source.keys else { return false }
+        for (key, value) in self {
+            guard let sourceValue = source[key] else { return false }
+            if !value.isContentEqual(to: sourceValue) { return false }
+        }
+        return true
     }
 }
 
-extension CollectionViewSectionViewModel: ContentIdentifiable where
-    CellViewModel: Hashable & Differentiable,
-    SupplementaryViewModel: Hashable & Differentiable {
+extension CollectionViewSectionViewModel: ContentEquatable where
+    SupplementaryViewModel: ContentEquatable {
+    public func isContentEqual(to source: Self) -> Bool {
+        return id.isContentEqual(to: source.id)
+            && supplementaryViewModels.isContentEqual(to: source.supplementaryViewModels)
+            && header.isContentEqual(to: source.header)
+            && footer.isContentEqual(to: source.footer)
+    }
+}
+
+extension CollectionViewSectionViewModel: ContentIdentifiable {
     public var differenceIdentifier: String {
         return id
     }
@@ -20,13 +30,12 @@ extension CollectionViewSectionViewModel: ContentIdentifiable where
 
 /// This conformance allows for DifferenceKit to create StagedChangeSet which in turn enable to animate changes.
 /// If you find yourself unable to create a changeSet using StagedChangeSet(source:,target:) with you ViewModel
-/// you should check on the conditions which are that your CellViewModel conforms to Hashable and Differentiable
-/// and the same goes for your SupplementaryViewModel.
-/// Note that Never conformance to Hashable is handled by Swift and conformance to Differentiable is handled
-/// by this pod.
+/// you should check on the conditions which are that your CellViewModel conforms to Differentiable
+/// and SupplementaryViewModel conforms to ContentEquatable.
+/// Note that Never conformance to Differentiable is handled by this pod.
 extension CollectionViewSectionViewModel: DifferentiableSection where
-    CellViewModel: Hashable & Differentiable,
-    SupplementaryViewModel: Hashable & Differentiable {
+    CellViewModel: Differentiable,
+    SupplementaryViewModel: ContentEquatable {
     public init<C>(source: CollectionViewSectionViewModel,
                    elements: C) where C: Swift.Collection, C.Element == CellViewModel {
         self.init(
